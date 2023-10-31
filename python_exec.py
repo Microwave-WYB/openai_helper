e9dd46d8d14df44deecd8aaee4b1c3cf28ad4d46
Yibo Wei <david_wyb2001@outlook.com>
2023-10-31 11:50:38 -0700

    Update python exec example

--- python_exec.py
+++ python_exec.py
@@ -18,7 +18,6 @@ def python_exec(code: str) -> str:
     """
     # Create a temporary file
     with tempfile.NamedTemporaryFile(suffix=".py", delete=False) as temp:
-        
         # Write the code to the file
         temp.write(code.encode())
         temp.close()
@@ -34,10 +33,4 @@ def python_exec(code: str) -> str:
 
     return output.decode()
 
-message = {"role": "user", "content": "Generate a random number between 1 and 10000"}
 chat = ChatSession(functions, model="gpt-4", verbose=True)
-print(
-    chat.send_messages([message], temperature=0, max_tokens=500)
-    .choices[0]
-    .message.content
-)
